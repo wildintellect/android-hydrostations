2e3905ea0619ea1ea2ee37cd94289129424d0718
Alex Mandel <tech@wildintellect.com>
2011-04-27 00:55:48 -0700

    App loads a map, good enough for tonight. TODO: figure out how to use the zoom in the emulator.

--- src/edu/ucdavis/ttp/hydrostations/stationmap.java
+++ src/edu/ucdavis/ttp/hydrostations/stationmap.java
@@ -7,14 +7,21 @@
  * */
 package edu.ucdavis.ttp.hydrostations;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
-public class stationmap extends Activity {
+public class stationmap extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
